88881c09123327b0bdbbcf519343dbb602bca324
Amir Bashiri <bashiri.net@gmail.com>
2023-01-17 00:44:31 +0330

    Add downloader.sh

---
+++ downloader.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# sudo apt install -y axel
+
+FILE="$1"
+if [ -z $FILE ]; then
+    if [ ! -f "list.txt" ]; then
+        echo "Name of file to download is needed!"
+        exit 1
+    fi
+    FILE="list.txt"
+fi
+
+CONNECTION="$2"
+[ -z $CONNECTION ] && CONNECTION=8
+
+if [ ! -f $FILE ]; then
+    echo
+    axel -acn $CONNECTION "$FILE"
+    exit 0
+fi
+
+while IFS= read -r LINK; do
+    if [ ! -z "$LINK" ]; then
+        echo
+        axel -acn $CONNECTION "$LINK"
+    fi
+done <"$FILE"
+
