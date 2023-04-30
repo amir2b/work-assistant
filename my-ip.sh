c9f80a888b88edee0dee16858fafa081c95a3781
Amir Bashiri <bashiri.dev@gmail.com>
2023-04-30 11:43:08 +0330

    fix my-ip

--- my-ip.sh
+++ my-ip.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-## Find last ip
-# curl https://api.myip.com
+set -e
+
+## Find local ip
+echo "Local IP:"
+ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n 1p
 
 ## Find public ip v4
+echo -e "\nPublic IP:"
 dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'
+# wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/"
 
 ## Find public ip v6
+# echo -e "\nPublic IP6:"
 # dig -6 TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'
 
