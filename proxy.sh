e0631cf3665e140a9161295d44356a21db013813
Amir Bashiri <bashiri.dev@gmail.com>
2025-06-11 14:20:01 +0330

    Fix proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -5,6 +5,7 @@ set -e
 
 export HTTP_PROXY=http://127.0.0.1:20171
 export HTTPS_PROXY=http://127.0.0.1:20171
+export NO_PROXY=localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24
 
 exec $@
 
